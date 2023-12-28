fb741f175c898b5dd748cffa2b0742ad39c42eef
Bảo Hiếu <l.b.hieu.vmp@gmail.com>
2023-12-28 21:47:02 +0700

    5 maps

--- src/Agent.py
+++ src/Agent.py
@@ -10,11 +10,11 @@ class Agent:
 
     # Check wall and move forward
     def move_foward(self, state):
-        if self.current_direction == Action.LEFT and state[self.current_state].left != 'Wall':
-            self.current_state = state[self.current_state].left
-        elif self.current_direction == Action.RIGHT and state[self.current_state].right != 'Wall':
-            self.current_state = state[self.current_state].right
-        elif self.current_direction == Action.UP and state[self.current_state].up != 'Wall':
+        if self.current_direction == Action.UP and state[self.current_state].up != 'Wall':
             self.current_state = state[self.current_state].up
+        elif self.current_direction == Action.LEFT and state[self.current_state].left != 'Wall':
+            self.current_state = state[self.current_state].left
         elif self.current_direction == Action.DOWN and state[self.current_state].down != 'Wall':
             self.current_state = state[self.current_state].down
+        elif self.current_direction == Action.RIGHT and state[self.current_state].right != 'Wall':
+            self.current_state = state[self.current_state].right
