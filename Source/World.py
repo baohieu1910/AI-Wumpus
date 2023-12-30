3193e022dbfe720ffd08b6ebc6045d923dc6b80b
Duy Anh Nguyen <duyanhnguyen71103@gmail.com>
2023-12-30 10:12:17 +0700

    update world

--- Source/World.py
+++ Source/World.py
@@ -3,7 +3,7 @@ from random import randrange
 
 
 class WumpusWorld:
-    def __init__(self):
+    def __init__(self, filename):
         """Initialize an empty board"""
         self.row = 0
         self.col = 0
@@ -11,6 +11,44 @@ class WumpusWorld:
         self.numWumpus = 0
         self.listTiles = []
         self.doorPos = None
+        with open(filename, 'r') as f:
+            lines = f.read().splitlines()
+            lines = lines[1:]
+            self.row = len(lines)
+
+            tiles = []
+            for line in lines:
+                tiles.append(line.split('.'))
+            self.col = len(tiles[0])
+
+            # Empty tiles map
+            for i in range(self.row):
+                tile_line = []
+                for j in range(self.col):
+                    tile_line.append(Tile())
+                self.listTiles.append(tile_line)
+
+            # Tile's objects
+            for i in range(self.row):
+                for j in range(self.col):
+                    if 'A' in tiles[i][j]:
+                        (self.listTiles[i][j]).setPlayer()
+                        self.doorPos = (i, j)
+                    if 'G' in tiles[i][j]:
+                        (self.listTiles[i][j]).setGold()
+                        self.numGold += 1
+                    if 'P' in tiles[i][j]:
+                        (self.listTiles[i][j]).setPit()
+                        adj = self.getAdjacents(i, j)
+                        for a in adj:
+                            (self.listTiles[a[0]][a[1]]).setBreeze()
+                    if 'W' in tiles[i][j]:
+                        (self.listTiles[i][j]).setWumpus()
+                        self.numWumpus += 1
+                        adj = self.getAdjacents(i, j)
+                        for a in adj:
+                            (self.listTiles[a[0]][a[1]]).setStench()
+
 
     def getAdjacents(self, i, j):
         adj = []
@@ -24,52 +62,11 @@ class WumpusWorld:
             adj.append((i, j + 1))
         return adj
 
-    def readMap(self, filename):
-        try:
-            with open(filename, 'r') as f:
-                lines = f.read().splitlines()
-                lines = lines[1:]
-                self.row = len(lines)
-
-                tiles = []
-                for line in lines:
-                    tiles.append(line.split('.'))
-                self.col = len(tiles[0])
-
-                # Empty tiles map
-                for i in range(self.row):
-                    tile_line = []
-                    for j in range(self.col):
-                        tile_line.append(Tile())
-                    self.listTiles.append(tile_line)
-
-                # Tile's objects
-                for i in range(self.row):
-                    for j in range(self.col):
-                        if 'A' in tiles[i][j]:
-                            (self.listTiles[i][j]).setPlayer()
-                            self.doorPos = (i, j)
-                        if 'G' in tiles[i][j]:
-                            (self.listTiles[i][j]).setGold()
-                            self.numGold += 1
-                        if 'P' in tiles[i][j]:
-                            (self.listTiles[i][j]).setPit()
-                            adj = self.getAdjacents(i, j)
-                            for a in adj:
-                                (self.listTiles[a[0]][a[1]]).setBreeze()
-                        if 'W' in tiles[i][j]:
-                            (self.listTiles[i][j]).setWumpus()
-                            self.numWumpus += 1
-                            adj = self.getAdjacents(i, j)
-                            for a in adj:
-                                (self.listTiles[a[0]][a[1]]).setStench()
-
-        except IOError:
-            return None
 
     def grabGold(self, i, j):
         self.numGold -= 1
         self.listTiles[i][j].removeGold()
+
 
     def killWumpus(self, i, j):
         self.numWumpus -= 1
@@ -79,12 +76,15 @@ class WumpusWorld:
             if self.listTiles[a[0]][a[1]].getStench():
                 self.listTiles[a[0]][a[1]].removeStench()
 
+
     def movePlayer(self, before_i, before_j, after_i, after_j):
         self.listTiles[before_i][before_j].removePlayer()
         self.listTiles[after_i][after_j].setPlayer()
 
+
     def leftGold(self):
         return bool(self.numGold)
+
 
     def leftWumpus(self):
         return bool(self.numWumpus)
--- Source/main.py
+++ Source/main.py
@@ -4,12 +4,11 @@ from Menu import Menu
 from Constants import *
 
 if __name__ == "__main__":
-    wumpus_world = WumpusWorld()
     menu = Menu()
     state = menu.run()
     map = int(state[-1])
 
-    wumpus_world.readMap(MAP_LIST[map - 1])
+    wumpus_world = WumpusWorld(MAP_LIST[map - 1])
     board = Board(wumpus_world)
     board.createWorld()
     board.run()
